fd7593270bf8e884c625550ad14723a14ee3bc5c
PHI-Case-Studies <herman.tolentino@gmail.com>
2019-06-30 23:07:33 -0500

    revised setup file for az nb

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -11,7 +11,7 @@ source /home/nbuser/anaconda3_420/bin/activate
 
 # Install packages
 conda update -c conda-forge conda conda-build
-conda install -y -c conda-forge folium=0.9.1
+conda install -y -c conda-forge folium=0.9.1 jinja2=2.10*
 
 pip install --upgrade pip
 pip install pandas==0.24.2
