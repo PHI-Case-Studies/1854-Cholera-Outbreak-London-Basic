cb65d3009eb0d4ffa2d2a14b547b8abc7a26f1e5
PHI-Case-Studies <herman.tolentino@gmail.com>
2019-06-30 22:28:03 -0500

    revised setup file for az nb

--- aznbsetup.sh
+++ aznbsetup.sh
@@ -1,3 +1,19 @@
 #!/bin/bash
-/home/nbuser/anaconda3_420/bin/conda install -y \
-    pandas=0.24* folium=0.9.1
+
+# Activate environment
+source /home/nbuser/anaconda3_420/bin/activate
+
+# Set up proxy
+#http_proxy=http://webproxy:3128
+#https_proxy=http://webproxy:3128
+#export http_proxy
+#export https_proxy
+
+# Install packages
+conda update -c conda-forge conda conda-build
+conda install -y -c conda-forge folium=0.9.1
+
+pip install --upgrade pip
+pip install pandas==0.24.2
+
+source /home/nbuser/anaconda3_420/bin/deactivate
